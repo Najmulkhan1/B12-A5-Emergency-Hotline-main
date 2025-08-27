03498bdaf2947536644cd3b191d8a2e588a702ba
Najmulkhan1 <najmulislam624@gmail.com>
2025-08-28 01:57:32 +0600

    implement Call Button functionality with coin deduction and history tracking

--- script.js
+++ script.js
@@ -24,5 +24,74 @@ for (let click of hardClick) {
   });
 }
 
+for (let click of callBtn) {
+  click.addEventListener('click', function() {
+
+    const currentCoin = document.getElementById('current-coin').innerText
+    const currentCoinConverted = parseInt(currentCoin)
+
+    const mainusCoin = currentCoinConverted - 20
+
+    if(mainusCoin < 20){
+        alert("No coin available")
+        return
+    }
+
+    document.getElementById('current-coin').innerText = mainusCoin
+
+    const now = new Date();
+    const Nowtime = now.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+
+
+
+    const historyParent = document.getElementById('history-parent');
+
+    
+    const card = click.closest('.card');  
+    const cardTitleText = card.querySelector('.card-title').innerText; 
+    const cardNumberText = card.querySelector('.card-num').innerText
+
+    
+    const newCard = document.createElement('div');
+    newCard.className = 'card bg-gray-100 card-sm shadow-sm mb-2';
+
+   
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body flex flex-row justify-between items-center';
+
+    const leftDiv = document.createElement('div');
+    const cardTitle = document.createElement('h2');
+    cardTitle.className = 'card-title';
+    cardTitle.innerText = cardTitleText;
+    const cardNumber = document.createElement('h3');
+    cardNumber.innerText = cardNumberText;
+
+    leftDiv.appendChild(cardTitle);
+    leftDiv.appendChild(cardNumber);
+
+    const dateEl = document.createElement('h4');
+    dateEl.innerText = Nowtime;
+
+    cardBody.appendChild(leftDiv);
+    cardBody.appendChild(dateEl);
+
+    newCard.appendChild(cardBody);
+
+    historyParent.appendChild(newCard);
+
+    alert("Calling: " + cardTitleText + " | " + cardNumber.innerText);
+  });
+}
+
+// history clear
+
+document.getElementById('clear-btn').addEventListener('click', function(){
+    document.getElementById('history-parent').innerText = ' '
+})
 
 
