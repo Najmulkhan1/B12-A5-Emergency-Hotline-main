88ba37252b2be0fd1bbcc3328926f4e9c365a121
Najmulkhan1 <najmulislam624@gmail.com>
2025-08-29 14:35:32 +0600

    ans the question

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ function innerTxt(id) {
 // select all elements with class "heard-click", "copy-btn"
 const hardClick = document.getElementsByClassName("heard-click");
 const copyBtn = document.getElementsByClassName("copy-btn");
-const callBtn = document.getElementsByClassName('call-btn')
+const callBtn = document.getElementsByClassName("call-btn");
 
-// loop
-
+// hardClick functionality
 for (let click of hardClick) {
   click.addEventListener("click", function () {
     // const hard = document.getElementById('current-hart').innerText
@@ -24,57 +23,52 @@ for (let click of hardClick) {
   });
 }
 
+// CallBtn functionality
 for (let click of callBtn) {
-  click.addEventListener('click', function() {
+  click.addEventListener("click", function () {
+    const currentCoin = document.getElementById("current-coin").innerText;
+    const currentCoinConverted = parseInt(currentCoin);
 
-    const currentCoin = document.getElementById('current-coin').innerText
-    const currentCoinConverted = parseInt(currentCoin)
+    const mainusCoin = currentCoinConverted - 20;
 
-    const mainusCoin = currentCoinConverted - 20
-
-    if(mainusCoin < 20){
-        alert("No coin available")
-        return
+    if (mainusCoin < 0) {
+      alert("❌আপনার পর্যাপ্ত কয়েন নেই কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+      return;
     }
 
-    document.getElementById('current-coin').innerText = mainusCoin
+    document.getElementById("current-coin").innerText = mainusCoin;
 
-    const now = new Date();
-    const Nowtime = now.toLocaleTimeString("en-US", {
+    let now = new Date();
+    let Nowtime = now.toLocaleTimeString("en-US", {
       hour: "2-digit",
       minute: "2-digit",
       second: "2-digit",
       hour12: true,
     });
 
+    const historyParent = document.getElementById("history-parent");
 
+    const card = click.closest(".card");
+    const cardTitleText = card.querySelector(".card-title").innerText;
+    const cardNumberText = card.querySelector(".card-num").innerText;
 
-    const historyParent = document.getElementById('history-parent');
+    const newCard = document.createElement("div");
+    newCard.className = "card bg-gray-100 card-sm shadow-sm mb-2";
 
-    
-    const card = click.closest('.card');  
-    const cardTitleText = card.querySelector('.card-title').innerText; 
-    const cardNumberText = card.querySelector('.card-num').innerText
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body flex flex-row justify-between items-center";
 
-    
-    const newCard = document.createElement('div');
-    newCard.className = 'card bg-gray-100 card-sm shadow-sm mb-2';
-
-   
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body flex flex-row justify-between items-center';
-
-    const leftDiv = document.createElement('div');
-    const cardTitle = document.createElement('h2');
-    cardTitle.className = 'card-title';
+    const leftDiv = document.createElement("div");
+    const cardTitle = document.createElement("h2");
+    cardTitle.className = "card-title";
     cardTitle.innerText = cardTitleText;
-    const cardNumber = document.createElement('h3');
+    const cardNumber = document.createElement("h3");
     cardNumber.innerText = cardNumberText;
 
     leftDiv.appendChild(cardTitle);
     leftDiv.appendChild(cardNumber);
 
-    const dateEl = document.createElement('h4');
+    const dateEl = document.createElement("h4");
     dateEl.innerText = Nowtime;
 
     cardBody.appendChild(leftDiv);
@@ -84,14 +78,53 @@ for (let click of callBtn) {
 
     historyParent.appendChild(newCard);
 
-    alert("Calling: " + cardTitleText + " | " + cardNumber.innerText);
+    alert("📞 Calling: " + cardTitleText + " | " + cardNumber.innerText);
   });
 }
 
 // history clear
 
-document.getElementById('clear-btn').addEventListener('click', function(){
-    document.getElementById('history-parent').innerText = ' '
-})
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("history-parent").innerText = " ";
+});
+
+// copyBtn functionality
+for (let click of copyBtn) {
+  click.addEventListener("click", function () {
+
+    const currentCopy = innerTxt('current-copy')
+    let currentCopyConverted = parseInt(currentCopy)
+    
+    currentCopyConverted++
+
+    document.getElementById('current-copy').innerText = currentCopyConverted
+
+    const card = click.closest('.card')
+    const cardNumber = card.querySelector('.card-num')
+    const cardNumberText = cardNumber.innerText.trim()
 
 
+    // click.disabled = true
+    // const originalLabel = click.innerHTML
+
+
+    navigator.clipboard.writeText(cardNumberText)
+
+    alert('নাম্বর কপি হয়েছে: ' + cardNumberText)
+    // try {
+    //     if (navigator.clipboard && window.isSecureContext) {
+    //             navigator.clipboard.writeText(cardNumberText)
+    //     }else{
+    //         console.log('error');
+            
+    //     }
+
+    // } catch (error) {
+    //     console.log("error");
+        
+    // }
+    
+    
+
+  });
+}
